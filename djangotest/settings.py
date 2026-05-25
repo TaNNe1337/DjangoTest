e54550a709a0217eca78ccf7d90086894a30c4f3
niels <niels.wiesner@web.de>
2026-05-25 14:24:07 +0200

    add static files

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'polls/static/'
 CSRF_TRUSTED_ORIGINS = [
     'https://iseproject05.informatik.htw-dresden.de:8001',
     'http://iseproject05.informatik.htw-dresden.de:8001',
